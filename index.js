1bd42d9324b2d76778704e198cad9880193fc348
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2020-10-09 16:47:05 +0330

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const request = require('request');
+
+
+const options = {
+    url: 'https://namnak.com',
+    headers: {
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
+    }
+}
+
+request(options, function (error, response, body) {
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML
+});
